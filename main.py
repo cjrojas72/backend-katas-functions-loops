2accb1d4b3671a58f0129d2e8cd695e6fbcff0b9
Christian Rojas <cjrojas72@gmail.com>
2020-04-05 19:26:36 -0400

    funcs done

--- main.py
+++ main.py
@@ -1,37 +1,60 @@
 #!/usr/bin/env python
 """Implements math functions without using operators except for '+' and '-' """
+import operator
 
-__author__ = "???"
+__author__ = "Christian Rojas"
 
 
 def add(x, y):
     """Add two integers. Handles negative values."""
-    # your code here
-    return
+    return x + y
 
 
 def multiply(x, y):
     """Multiply x with y. Handles negative values of x or y."""
-    # your code here
-    return
+    value = 0
+
+    for num in range(abs(y)):
+        value = add(value, abs(x))
+
+    if x < 0 and y < 0:
+        return value
+    elif x < 0 or y < 0:
+        return -value
+    else:
+        return value
 
 
 def power(x, n):
     """Raise x to power n, where n >= 0"""
-    # your code here
-    return
+    value = x
+    if n == 0:
+        return 1
+    else:
+        for num in range(1, n):
+            value = multiply(value, x)
+    return value
 
 
 def factorial(x):
     """Compute factorial of x, where x > 0"""
-    # your code here
-    return
+    f = 1
+    if x >= 1:
+        for i in range(1, add(x, 1)):
+            f = multiply(f, i)
+    return f
 
 
 def fibonacci(n):
     """Compute the nth term of fibonacci sequence"""
-    # your code here
-    return
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        return 0
+    elif n == 2 or n == 1:
+        return 1
+    else:
+        return add(fibonacci(n-1), fibonacci(n-2))
 
 
 if __name__ == '__main__':
